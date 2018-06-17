8c7c466b409e6de8ad9b6f370629cb9d3265104e
Nathen Harvey <nharvey@chef.io>
2018-06-17 14:24:23 -0400

    Wrap the chef-client cookbook
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ cookbooks/my_chef_client/attributes/default.rb
@@ -0,0 +1,3 @@
+default['chef_client']['interval']    = '120'
+default['chef_client']['splay']       = '20'
+
--- roles/opsworks-example-role.rb
+++ roles/opsworks-example-role.rb
@@ -2,8 +2,8 @@ name "opsworks-example-role"
 description "This role specifies all recipes described in the starter kit guide (README.md)"
 
 run_list(
-  "recipe[chef-client]",
-  "recipe[apache2]",
   "recipe[opsworks-audit]"
   # "recipe[ssh-hardening]"
+  "recipe[my_chef_client]",
+  # "recipe[apache2]",
 )
