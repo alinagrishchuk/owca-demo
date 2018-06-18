93201c29a9f8c0a0a46c55c2dd7394e490df206b
Nathen Harvey <nharvey@chef.io>
2018-06-17 23:50:58 -0400

    Allow an alternate location for the starter files
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- setup.sh
+++ setup.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # Usage and help
-USAGE="Usage: $0 [owca-name]"
+USAGE="Usage: $0 [owca-name] [base directory for starter kit and credentials file (optional)]"
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -eq 0 ]]; then
   echo -e $USAGE
   exit 1
+fi
+
+if [[ $# -eq 1 ]]; then
+  BASE_DIR=~/Downloads
+fi
+
+if [[ $# -eq 2 ]]; then
+  BASE_DIR=$2
 fi
 
 # Variables
 OWCA_NAME=$1
 
 # Unzip the starter kit
-cp ~/Downloads/"$OWCA_NAME"_starter_kit.zip .
+cp "$BASE_DIR"/"$OWCA_NAME"_starter_kit.zip .
 unzip "$OWCA_NAME"_starter_kit.zip 
-mv "$OWCA_NAME"_starter_kit.zip ~/Downloads
+mv "$OWCA_NAME"_starter_kit.zip "$BASE_DIR"
 
 # Move the files in place
 cp -rf $OWCA_NAME*/.chef/ ./.chef/
@@ -30,6 +38,6 @@ sed -i '' "s/CHEF_SERVER_ENDPOINT=.*/$CHEF_SERVER_ENDPOINT/g" userdata-rhel.sh
 sed -i '' "s/CHEF_SERVER_ENDPOINT=.*/$CHEF_SERVER_ENDPOINT/g" userdata-ubuntu.sh
 
 # Remove the unzipped files
-rm -rf $OWCA_NAME*/
+rm -rf $OWCA_NAME-*/
 
-echo "The Admin password is `cat ~/Downloads/\"$OWCA_NAME\"_credentials.csv | cut -d ',' -f2`"
+echo "The Admin password is `cat "$BASE_DIR"/\"$OWCA_NAME\"_credentials.csv | cut -d ',' -f2`"
